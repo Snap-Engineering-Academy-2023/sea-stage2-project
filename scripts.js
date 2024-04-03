f5ad6fa06ac9b6bacc607d9581725bae71468237
bakerfugu <1bakersharp@gmail.com>
2024-04-02 17:53:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
+/**
+ * Data Catalog Project Starter Code - SEA Stage 2
+ *
+ * This file is where you should be doing most of your work. You should
+ * also make changes to the HTML and CSS files, but we want you to prioritize
+ * demonstrating your understanding of data structures, and you'll do that
+ * with the JavaScript code you write in this file.
+ * 
+ * The comments in this file are only to help you learn how the starter code
+ * works. The instructions for the project are in the README. That said, here
+ * are the three things you should do first to learn about the starter code:
+ * - 1 - Change something small in index.html or style.css, then reload your 
+ *    browser and make sure you can see that change. 
+ * - 2 - On your browser, right click anywhere on the page and select
+ *    "Inspect" to open the browser developer tools. Then, go to the "console"
+ *    tab in the new window that opened up. This console is where you will see
+ *    JavaScript errors and logs, which is extremely helpful for debugging.
+ *    (These instructions assume you're using Chrome, opening developer tools
+ *    may be different on other browsers. We suggest using Chrome.)
+ * - 3 - Add another string to the titles array a few lines down. Reload your
+ *    browser and observe what happens. You should see a fourth "card" appear
+ *    with the string you added to the array, but a broken image.
+ * 
+ */
+
+
 const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles). 
-// Try adding another title to the array and see what happens.
+// This is an array of strings (TV show titles)
 let titles = [
     "Fresh Prince of Bel Air",
     "Curb Your Enthusiasm",
     "East Los High"
 ];
+// Your final submission should have much more data than this, and 
+// you should use more than just an array of strings to store it all.
+
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -19,7 +47,8 @@ function showCards() {
     for (let i = 0; i < titles.length; i++) {
         let title = titles[i];
 
-        // This code doesn't scale well, you should find another way to do it
+        // This part of the code doesn't scale very well! After you add your
+        // own data, you'll need to do something totally different here.
         let imageURL = "";
         if (i == 0) {
             imageURL = FRESH_PRINCE_URL;
